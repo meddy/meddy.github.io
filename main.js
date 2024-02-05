e47aa878a90900a7ca354862ed87eeed3ac56f34
Max Eddy <maxiscool@gmail.com>
2024-02-05 11:01:55 -0800

    Remove cringe and make index.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-require('./main.scss');
-require('./portrait.jpg');
-require('ionicons/dist/ionicons/svg/logo-linkedin.svg');
-require('ionicons/dist/ionicons/svg/logo-github.svg')
-require('ionicons/dist/ionicons/svg/md-document.svg');
+require("./main.scss");
+require("ionicons/dist/ionicons/svg/logo-linkedin.svg");
+require("ionicons/dist/ionicons/svg/logo-github.svg");
